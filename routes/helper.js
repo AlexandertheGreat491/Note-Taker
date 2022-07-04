cbc0965cc30b00d93aa69b5424253831f1cb3ce0
agvan <agvandyke25@gmail.com>
2022-07-04 12:40:09 -0500

    added module exports for readFromFile, writeToFile, & readAndAppend

--- routes/helper.js
+++ routes/helper.js
@@ -15,14 +15,14 @@ const writeToFile = (destination, content) =>
         err ? console.error(err) : console.info(`\nData written to ${destination}`)
     );
 
- /**
-  * Function to read data from a given file & append content.
-  * @param {object} content the content you want to append to the file
-  * @param {string} file the path to the file you want to save to.
-  * @returns {void} Nothing
-  */   
+/**
+ * Function to read data from a given file & append content.
+ * @param {object} content the content you want to append to the file
+ * @param {string} file the path to the file you want to save to.
+ * @returns {void} Nothing
+ */
 
- const readAndAppend = (content, file) => {
+const readAndAppend = (content, file) => {
     fs.readFile(file, 'utf8', (err, data) => {
         if (err) {
             console.error(err);
@@ -32,4 +32,6 @@ const writeToFile = (destination, content) =>
             writeToFile(file, parsedData);
         }
     });
- };
+};
+
+module.exports = { readFromFile, writeToFile, readAndAppend };
