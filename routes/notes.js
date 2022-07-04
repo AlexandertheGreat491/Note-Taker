ec59d96d2e38bc7040778cd3838318fff513313f
agvan <agvandyke25@gmail.com>
2022-07-04 12:06:18 -0500

    added a comment

--- routes/notes.js
+++ routes/notes.js
@@ -6,3 +6,5 @@ const notes = require("express").Router();
 notes.get("/", (req, res) => {
     readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
+
+//Delete route for a particular note
