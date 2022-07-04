71e6451ab93ca23b8c79dfdb0e681c784d5c0ff6
agvan <agvandyke25@gmail.com>
2022-07-04 13:08:59 -0500

    updated server.js code & deleted helper.js file

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 //require statements
 const express = require('express');
 const path = require('path');
-const api = require('./routes/index.js');
-const { middle } = require('./middleware/middle');
 //variables that allow the server to run through command line
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -12,20 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//Route for homepage
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
-//Route for the notes page
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
-//Wildcard route for the homepage
-app.get('/notes', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
 
 //listener for PORT. PORT connected successfully to the server.
 app.listen(PORT, () => {
