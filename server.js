d938b3cbf556f218f4f33065cdca051ab0faf50f
agvan <agvandyke25@gmail.com>
2022-07-04 13:47:48 -0500

    fixed routes and gets and post

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
-//require statements
 const express = require('express');
 const path = require('path');
-//variables that allow the server to run through command line
-const app = express();
+const api = require('./routes/index.js');
+
 const PORT = process.env.PORT || 3001;
 
-//Middleware that will parse the JSON & urlencoded form data.
-app.use(express.urlencoded({ extended: true }));
+const app = express();
+
+
+// Middleware for parsing JSON and urlencoded form data 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
+
 app.use(express.static('public'));
 
+// GET Route for homepage
+app.get('/', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Wildcard Route for homepage
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 //listener for PORT. PORT connected successfully to the server.
 app.listen(PORT, () => {
