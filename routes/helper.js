8436803d5a41bca5afda7b2aa957e14a946b727a
agvan <agvandyke25@gmail.com>
2022-07-04 12:38:36 -0500

    added writeToFile & readAndAppend functions

--- routes/helper.js
+++ routes/helper.js
@@ -10,3 +10,26 @@ const readFromFile = util.promisify(fs.readFile);
  * @returns {void} Nothing
  */
 
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+ /**
+  * Function to read data from a given file & append content.
+  * @param {object} content the content you want to append to the file
+  * @param {string} file the path to the file you want to save to.
+  * @returns {void} Nothing
+  */   
+
+ const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parseData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+ };
