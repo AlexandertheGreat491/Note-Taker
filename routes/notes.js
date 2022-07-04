b474487ba3612172720252122f4d62a0a129c94c
agvan <agvandyke25@gmail.com>
2022-07-04 12:17:32 -0500

    added POST route for new note

--- routes/notes.js
+++ routes/notes.js
@@ -10,15 +10,36 @@ notes.get("/", (req, res) => {
 //Delete route for a particular note
 notes.delete("/:id", (req, res) => {
     readFromFile("./db/db.json")
-    .then((data) => JSON.parse(data))
-    .then((json) => {
-        //Createsnew array of notes except for the one with the ID in the URL.
-        const result = json.filter((note) => note.id !== noteId);
+        .then((data) => JSON.parse(data))
+        .then((json) => {
+            //Createsnew array of notes except for the one with the ID in the URL.
+            const result = json.filter((note) => note.id !== noteId);
 
-        //Saves the array to the filesystem
-        writeToFile("./db/db.json", result);
+            //Saves the array to the filesystem
+            writeToFile("./db/db.json", result);
 
-        //Respond to the delete request.
-        res.json(`Item ${noteId} has been deleted`);
-    });
+            //Respond to the delete request.
+            res.json(`Item ${noteId} has been deleted`);
+        });
 });
+
+//POST Route for a new note
+notes.post("/", (req, res) => {
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if (req.body) {
+        const newnote = {
+            title,
+            text,
+            id: 2,
+        };
+
+        readAndAppend(newnote, "./db/db.json");
+        res.json(`Note added successfully!`);
+    } else {
+        res.error("Note could not be added.");
+    }
+});
+
