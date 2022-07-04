c042527bde3e666b863eabfab84fe625df26ce09
agvan <agvandyke25@gmail.com>
2022-07-04 12:11:08 -0500

    added delete route

--- routes/notes.js
+++ routes/notes.js
@@ -8,3 +8,17 @@ notes.get("/", (req, res) => {
 });
 
 //Delete route for a particular note
+notes.delete("/:id", (req, res) => {
+    readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+        //Createsnew array of notes except for the one with the ID in the URL.
+        const result = json.filter((note) => note.id !== noteId);
+
+        //Saves the array to the filesystem
+        writeToFile("./db/db.json", result);
+
+        //Respond to the delete request.
+        res.json(`Item ${noteId} has been deleted`);
+    });
+});
