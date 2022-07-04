6e66850e3ae2c6c095994f5b7eb5ba38ed13efd0
agvan <agvandyke25@gmail.com>
2022-07-04 13:38:28 -0500

    updated static to public

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3001;
 //Middleware that will parse the JSON & urlencoded form data.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(_dirname));
+app.use(express.static('public'));
 
 
 //listener for PORT. PORT connected successfully to the server.
