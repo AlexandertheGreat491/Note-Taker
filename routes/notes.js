4bd9ed1fe885fc20366637a78fa0f416329ca89e
agvan <agvandyke25@gmail.com>
2022-07-04 12:05:14 -0500

    adding GET Route for retrieving all the notes

---
+++ routes/notes.js
@@ -0,0 +1,8 @@
+const notes = require("express").Router();
+
+
+//GET for retrieving the notes
+
+notes.get("/", (req, res) => {
+    readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
