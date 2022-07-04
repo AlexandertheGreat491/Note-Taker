1a06f552cce17b39b70110ae95178efc6653a67a
agvan <agvandyke25@gmail.com>
2022-07-04 11:01:14 -0500

    finished adding routes & added a comment

--- server.js
+++ server.js
@@ -21,6 +21,11 @@ app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+//Wildcard route for the homepage
+app.get('/notes', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
 //listener for PORT. PORT connected successfully to the server.
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
