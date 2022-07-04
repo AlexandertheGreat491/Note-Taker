eefcaaf484389e271b0bce5dad8d9e4221538d5d
agvan <agvandyke25@gmail.com>
2022-07-04 12:29:05 -0500

    added readFromFile variable

--- routes/helper.js
+++ routes/helper.js
@@ -1,2 +1,12 @@
 const fs = require('fs');
 const util = require('util');
+
+// Promise version of fs.readFile
+const readFromFile = util.promisify(fs.readFile);
+/**
+ * Function to write data to the JSON file given a destination and some content.
+ * @param {string} destinations the file you want to write to.
+ * @param {object} content the content you want to write the file.
+ * @returns {void} Nothing
+ */
+
