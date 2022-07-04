680f3626f993845af8e4301c5ef52fe0e647a0ea
agvan <agvandyke25@gmail.com>
2022-07-04 11:59:48 -0500

    adding variables and module exports

--- routes/index.js
+++ routes/index.js
@@ -3,3 +3,9 @@ const express = require('express');
 //importing routers
 
 const notesRouter = require.apply('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+module.exports = app;
